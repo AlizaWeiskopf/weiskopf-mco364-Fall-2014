cf7526a05b8ff19100ef30dbd2e54eb95bf1e67a
AlizaWeiskopf <aweiskopf1@gmail.com>
2014-12-02 22:32:40 -0500

    Added comment

--- src/weiskopf/paint/Canvas.java
+++ src/weiskopf/paint/Canvas.java
@@ -11,43 +11,42 @@ import java.awt.image.BufferedImage;
 
 import javax.swing.JComponent;
 
-public class Canvas extends JComponent implements MouseWheelListener{
-	
+public class Canvas extends JComponent implements MouseWheelListener {
+
 	private BufferedImage image;
 	private DrawListener listener;
 	private DetailsPanel detailsPanel;
 	private OptionsPanel optionsPanel;
-	
+
 	private Color color;
 	private int stroke;
 	private int counter;
 	private boolean clear;
-	
 
 	public Canvas(Paint paint) {
+		// use buffered image b/c when you call repaint the canvas will clear -
+		// so need to draw to image and then draw that to canvas
 		image = new BufferedImage(800, 600, BufferedImage.TYPE_INT_ARGB);// A =
 																			// alpha
 																			// =
 																			// transparent
 																			// pixels
-		
 
 		setColor(Color.GREEN);
 		stroke = 5;
 		counter = 0;
-		clear = false;		
-		
+		clear = false;
+
 		addDrawListener(new DrawBrush(this));
 		addMouseWheelListener(this);
-		
+
 		optionsPanel = new OptionsPanel(this);
 		paint.add(optionsPanel, BorderLayout.NORTH);
-		
+
 		detailsPanel = new DetailsPanel(this);
 		paint.add(detailsPanel, BorderLayout.SOUTH);
 
 	}
-	
 
 	@Override
 	protected void paintComponent(Graphics g) {
@@ -56,11 +55,10 @@ public class Canvas extends JComponent implements MouseWheelListener{
 			super.paintComponent(g);
 			g.drawImage(image, 0, 0, null);
 			listener.drawPreview((Graphics2D) g);
-			
+
 		}
 
 	}
-
 
 	public Color getColor() {
 		return color;
@@ -78,33 +76,33 @@ public class Canvas extends JComponent implements MouseWheelListener{
 	public int getStrokeSize() {
 		return stroke;
 	}
-	
-	public BufferedImage getImage(){
+
+	public BufferedImage getImage() {
 		return image;
 	}
-	
-	public int getCounter(){
+
+	public int getCounter() {
 		return counter;
 	}
-	
-	public void incrementCounter(){
+
+	public void incrementCounter() {
 		counter++;
 	}
-	
-	public boolean getClear(){
+
+	public boolean getClear() {
 		return clear;
 	}
-	
-	public void resetClear(){
+
+	public void resetClear() {
 		clear = false;
 	}
-	
-	public void clear(){
+
+	public void clear() {
 		clear = true;
 		image = new BufferedImage(800, 600, BufferedImage.TYPE_INT_ARGB);
 		repaint();
 	}
-	
+
 	public void reset() {
 		counter = 0;
 	}
@@ -113,24 +111,24 @@ public class Canvas extends JComponent implements MouseWheelListener{
 	public void mouseWheelMoved(MouseWheelEvent e) {
 		incrementStroke(e.getPreciseWheelRotation());
 		int size = getStrokeSize();
-		if(size < 0){
+		if (size < 0) {
 			size = 0;
 		}
 		detailsPanel.getStrokeSize().setText("Stroke size: " + size);
-		
+
 	}
 
 	public void addDrawListener(DrawListener listener) {
 		this.removeMouseListener(this.listener);
 		this.removeMouseMotionListener(this.listener);
-		
-		this.listener =  listener;
+
+		this.listener = listener;
 		addMouseListener(listener);
 		addMouseMotionListener(listener);
-		
+
 	}
-	
-	public void setGraphicsDetails(Graphics2D g){
+
+	public void setGraphicsDetails(Graphics2D g) {
 
 		int stroke = getStrokeSize();
 		if (stroke < 0) {
